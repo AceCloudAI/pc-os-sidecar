a481318c1d711fcc84d27e911f1b0f0006960de0
Ayush Singh Srikoti <ayushsinghsrikoti@GGN1RTDSLPT089.local>
2025-09-18 19:49:22 +0530

    added region as query param

--- src/domains/openstack/nova/sub-domains/pci-devices/pci-device.controller.ts
+++ src/domains/openstack/nova/sub-domains/pci-devices/pci-device.controller.ts
@@ -1,15 +1,16 @@
 import {
-    BadRequestException,
-    ConflictException,
-    Controller,
-    Get,
-    HttpStatus,
-    NotFoundException,
-    Param,
-    Req,
-    Res,
-    UnauthorizedException,
-    UnprocessableEntityException,
+  BadRequestException,
+  ConflictException,
+  Controller,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Query,
+  Req,
+  Res,
+  UnauthorizedException,
+  UnprocessableEntityException,
 } from '@nestjs/common';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { ERROR_RESPONSE, SUCCESS } from 'src/common/constants';
@@ -19,78 +20,99 @@ import { PCIDeviceService } from './pci-device.service';
 
 @Controller('pci-devices')
 export class PCIDeviceController {
-    constructor(
-        private readonly pciDeviceService: PCIDeviceService,
-        private readonly logger: CustomLoggerService,
-    ) { }
+  constructor(
+    private readonly pciDeviceService: PCIDeviceService,
+    private readonly logger: CustomLoggerService,
+  ) {}
 
-    @Get('inventory/:region')
-    async getAvailableInventory(
-        @Req() req: FastifyRequest,
-        @Res() res: FastifyReply,
-        @Param('region') region: string,
-    ) {
-        try {
-            this.pciDeviceService.setPciDeviceRepository(region);
-            const inventory = await this.pciDeviceService.fetchAvailableInventory();
+  @Get()
+  async getAvailableInventory(
+    @Req() req: FastifyRequest,
+    @Res() res: FastifyReply,
+    @Query('region') region: string,
+  ) {
+    try {
+      this.pciDeviceService.setPciDeviceRepository(region);
+      const inventory = await this.pciDeviceService.fetchAvailableInventory();
 
-            return res.status(HttpStatus.OK).send({
-                error: false,
-                message: SUCCESS,
-                data: { inventory },
-            });
-        } catch (error) {
-            return this.handleError(error, req, res, this.getAvailableInventory.name);
-        }
+      return res.status(HttpStatus.OK).send({
+        error: false,
+        message: SUCCESS,
+        data: { inventory },
+      });
+    } catch (error) {
+      return this.handleError(error, req, res, this.getAvailableInventory.name);
     }
+  }
 
-    @Get('inventory/:region/product/:productId')
-    async getAvailableInventoryByProductId(
-        @Req() req: FastifyRequest,
-        @Res() res: FastifyReply,
-        @Param('region') region: string,
-        @Param('productId') productId: string,
-    ) {
-        try {
-            this.pciDeviceService.setPciDeviceRepository(region);
-            const count = await this.pciDeviceService.fetchAvailableInventoryByProductID(productId);
+  @Get('product/:productId')
+  async getAvailableInventoryByProductId(
+    @Req() req: FastifyRequest,
+    @Res() res: FastifyReply,
+    @Param('productId') productId: string,
+    @Query('region') region: string,
+  ) {
+    try {
+      this.pciDeviceService.setPciDeviceRepository(region);
+      const count =
+        await this.pciDeviceService.fetchAvailableInventoryByProductID(
+          productId,
+        );
 
-            return res.status(HttpStatus.OK).send({
-                error: false,
-                message: SUCCESS,
-                data: {
-                    productId,
-                    availableCount: count,
-                },
-            });
-        } catch (error) {
-            return this.handleError(error, req, res, this.getAvailableInventoryByProductId.name);
-        }
+      return res.status(HttpStatus.OK).send({
+        error: false,
+        message: SUCCESS,
+        data: {
+          productId,
+          availableCount: count,
+        },
+      });
+    } catch (error) {
+      return this.handleError(
+        error,
+        req,
+        res,
+        this.getAvailableInventoryByProductId.name,
+      );
     }
+  }
 
-    private handleError(error: unknown, req: FastifyRequest, res: FastifyReply, functionName: string) {
-        const errorObj: ErrorLogInterface = {
-            request: req,
-            error,
-            functionName: `PCIDeviceController/${functionName}`,
-        };
-        this.logger.errorLog(errorObj);
+  private handleError(
+    error: unknown,
+    req: FastifyRequest,
+    res: FastifyReply,
+    functionName: string,
+  ) {
+    const errorObj: ErrorLogInterface = {
+      request: req,
+      error,
+      functionName: `PCIDeviceController/${functionName}`,
+    };
+    this.logger.errorLog(errorObj);
 
-        if (error instanceof NotFoundException) {
-            return res.status(HttpStatus.NOT_FOUND).send({ error: true, message: error.message });
-        } else if (error instanceof UnauthorizedException) {
-            return res.status(HttpStatus.UNAUTHORIZED).send({ error: true, message: error.message });
-        } else if (error instanceof UnprocessableEntityException) {
-            return res.status(HttpStatus.UNPROCESSABLE_ENTITY).send({
-                error: true,
-                message: error.message,
-            });
-        } else if (error instanceof ConflictException) {
-            return res.status(HttpStatus.CONFLICT).send({ error: true, message: error.message });
-        } else if (error instanceof BadRequestException) {
-            return res.status(HttpStatus.BAD_REQUEST).send({ error: true, message: error.message });
-        } else {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ERROR_RESPONSE);
-        }
+    if (error instanceof NotFoundException) {
+      return res
+        .status(HttpStatus.NOT_FOUND)
+        .send({ error: true, message: error.message });
+    } else if (error instanceof UnauthorizedException) {
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .send({ error: true, message: error.message });
+    } else if (error instanceof UnprocessableEntityException) {
+      return res.status(HttpStatus.UNPROCESSABLE_ENTITY).send({
+        error: true,
+        message: error.message,
+      });
+    } else if (error instanceof ConflictException) {
+      return res
+        .status(HttpStatus.CONFLICT)
+        .send({ error: true, message: error.message });
+    } else if (error instanceof BadRequestException) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .send({ error: true, message: error.message });
+    } else {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ERROR_RESPONSE);
     }
+  }
 }
