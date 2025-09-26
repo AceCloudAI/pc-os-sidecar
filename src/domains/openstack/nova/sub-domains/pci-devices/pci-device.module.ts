d3d348574c6b1631cdff1ae4f25562d1bb35d7b3
Ayush Singh Srikoti <ayushsinghsrikoti@GGN1RTDSLPT089.local>
2025-09-26 19:34:59 +0530

    conosle log to get pcidevices

--- src/domains/openstack/nova/sub-domains/pci-devices/pci-device.module.ts
+++ src/domains/openstack/nova/sub-domains/pci-devices/pci-device.module.ts
@@ -5,12 +5,13 @@ import { PCIDeviceService } from './pci-device.service';
 import { PCIDeviceController } from './pci-device.controller';
 import { PCIDevice } from './entity/pci-device.entity';
 import { OPENSTACK_DB_CONNECTION_NAME } from 'src/common/constants';
+import { FlavorExtraSpec } from './entity/flavor.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([PCIDevice], OPENSTACK_DB_CONNECTION_NAME.noida),
-    TypeOrmModule.forFeature([PCIDevice], OPENSTACK_DB_CONNECTION_NAME.mumbai),
-    TypeOrmModule.forFeature([PCIDevice], OPENSTACK_DB_CONNECTION_NAME.atlanta),
+    TypeOrmModule.forFeature([PCIDevice, FlavorExtraSpec], OPENSTACK_DB_CONNECTION_NAME.noida),
+    TypeOrmModule.forFeature([PCIDevice, FlavorExtraSpec], OPENSTACK_DB_CONNECTION_NAME.mumbai),
+    TypeOrmModule.forFeature([PCIDevice, FlavorExtraSpec], OPENSTACK_DB_CONNECTION_NAME.atlanta),
     LoggerModule,
   ],
   controllers: [PCIDeviceController],
